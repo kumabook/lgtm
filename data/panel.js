4918c6f64f933d459de69eeca04f11a0ed4f5837
kumabook <kumabook@live.jp>
2014-05-15 01:28:42 +0900

    Implement main feature

--- data/panel.js
+++ data/panel.js
@@ -1,16 +1,60 @@
 const lgtmListView = document.getElementById('lgtm-list-view');
 const magin = 20;
-self.port.on('add-images', function(datum) {
-  datum.forEach(function(data) {
-    console.log('image ' + data);
-    var img = document.createElement('img');
-    img.width = window.innerWidth - magin;
-    img.src = data.imageUrl;
-    lgtmListView.appendChild(img);
-    img.addEventListener('click', function() {
-      console.log('click:' + data);
-      self.port.emit('select', data);
-    });
-  });
+var currentItems = null;
+var isLoading = false;
 
+var showProgress = function() {
+  isLoading = true;
+  var loader = document.createElement('img');
+  loader.id = 'loader';
+  loader.src = 'loader.gif';
+  loader.className = 'loader';
+  lgtmListView.appendChild(loader);
+};
+
+var hideProgress = function() {
+  isLoading = false;
+  var loader = document.getElementById('loader');
+  lgtmListView.removeChild(loader);
+};
+
+self.port.on('add-images', function(items) {
+  hideProgress();
+  currentItems = items;
+  items.forEach(function(item) {
+    console.log('image ' + item);
+    var img = document.createElement('img');
+    img.id = item.id;
+    img.width = window.innerWidth - magin;
+    img.className = 'image';
+    img.src = item.imageUrl;
+    lgtmListView.appendChild(img);
+    img._clickListener =  function() {
+      console.log('clicked:' + item);
+      document.getElementById('message').style.display = '';
+      self.port.emit('select', item);
+    };
+    img.addEventListener('click', img._clickListener);
+  });
+});
+
+self.port.on('show', function() {
+  console.log('show progress');
+  document.getElementById('message').style.display = 'none';
+  showProgress();
+});
+
+
+self.port.on('hide', function() {
+  if (currentItems !== null) {
+    currentItems.forEach(function(item) {
+      var img = document.getElementById(item.id);
+      img.removeEventListener('click', img._clickListener);
+    });
+    currentItems = null;
+  }
+  lgtmListView.innerHTML = '';
+  if (isLoading) {
+    self.port.emit('cancel');
+  }
 });
